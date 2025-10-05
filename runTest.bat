fcf66a89adeb4f6e93a67b74c186edc31695d0b0
GitIzzy72 <isaacebencorn.uni@gmail.com>
2025-10-05 19:15:40 -0400

    comparison implemented

--- runTest.bat
+++ runTest.bat
@@ -5,10 +5,21 @@ REM USAGE: test.bat <executable_file> <input_file.txt>
 REM FIRST PARAM: executable
 REM SECOND PARAM: input file - Defaults to test.txt
 
-REM Set the executable name, defaulting to "a.exe" if not provided
+REM check for compare flag "-c"
+
+set comp=%1
+
+if defined comp (
+    if "%comp%"=="-c" (
+        set comp=1
+        shift
+    )
+)
+
+REM Set the executable name
 if "%1"=="" (
     echo Error: No executable file specified.
-    echo Usage: %0 ^<executable_file^> ^<optional_input_file.txt^>
+    echo Usage: runTest ^<executable_file^> ^<optional_input_file.txt^>
     exit /b 1
 ) else (
     set "EXECUTABLE=%1"
@@ -27,18 +38,37 @@ if not exist %EXECUTABLE% (
     exit /b 1
 )
 
-set "INPUT_FILE=text_in/%INPUT_NAME%"
+REM file location setting
+set "BASE_NAME=%INPUT_NAME:.txt=%"
 
-REM Create a output file name
-set OUTPUT_FILE=%INPUT_FILE:.txt=_output.txt%
-set OUTPUT_FILE=%OUTPUT_FILE:in=out%
+set "INPUT_FOLDER=text_in"
+set "OUTPUT_FOLDER=text_out"
+set "EXPECTED_FOLDER=text_expected"
+
+REM set input, expected, output
+set "INPUT_FILE=%INPUT_FOLDER%\%INPUT_NAME%"
+set "OUTPUT_FILE=%OUTPUT_FOLDER%\%BASE_NAME%_output.txt"
+if not x%BASE_NAME:input=%==x%BASE_NAME% (
+    set "EXPECTED_FILE=%EXPECTED_FOLDER%\%INPUT_NAME:input=answer%"
+) else (
+    set "EXPECTED_FILE=%EXPECTED_FOLDER%\%BASE_NAME%_expected.txt"
+) 
 
 REM Check if input file exists
-if not exist test_in/%INPUT_FILE% (
+if not exist %INPUT_FILE% (
     echo Error: Input file not found
     if "%2"=="" (
         echo Default value for input file is 'test.txt'
     )
+    exit /b 1
+)
+
+REM file to compare is not provided in variables! 
+REM        BUT if flag is checked command WILL check if it exists or error otherwise
+
+if not exist %EXPECTED_FILE% (
+    echo Compare flag was checked but a file to compare does not exist
+    echo file should be: test_exepected/^<input^>_expected.txt
     exit /b 1
 )
 
@@ -53,4 +83,19 @@ if errorlevel 1 (
 ) else (
     echo Test completed
     echo Output saved to: %OUTPUT_FILE%
+
+    REM compare if the flag is set
+    if %comp%==1 (
+        echo --- comparing expected to output ---
+        fc "%OUTPUT_FILE%" "%EXPECTED_FILE%" > nul 
+        if errorlevel 2 (
+            echo There was a problem comparing %EXPECTED_FILE% and %OUTPUT_FILE%
+        ) else if errorlevel 1 (
+            echo Comparison Failed: 
+            echo     %EXPECTED_FILE% and %OUTPUT_FILE% are NOT identical
+        ) else (
+            echo Comparison successful:
+            echo     %EXPECTED_FILE% and %OUTPUT_FILE% are identical!
+        )
+    )
 )
